2b8cab4583b53949a2026d35b823468d81818391
bielecro <Robert.Bielecki@arhs-spikeseed.com>
2015-10-27 14:05:54 +0100

    framework update: ocsp revocation reason optional

--- dss-spi/src/main/java/eu/europa/esig/dss/x509/OCSPToken.java
+++ dss-spi/src/main/java/eu/europa/esig/dss/x509/OCSPToken.java
@@ -1,19 +1,19 @@
 /**
  * DSS - Digital Signature Services
  * Copyright (C) 2015 European Commission, provided under the CEF programme
- *
+ * <p/>
  * This file is part of the "DSS - Digital Signature Services" project.
- *
+ * <p/>
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- *
+ * <p/>
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- *
+ * <p/>
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -108,9 +108,14 @@ public class OCSPToken extends RevocationToken {
 			final RevokedStatus revokedStatus = (RevokedStatus) certStatus;
 			status = false;
 			revocationDate = revokedStatus.getRevocationTime();
-			final int reasonId = revokedStatus.getRevocationReason();
-			final CRLReason crlReason = CRLReason.lookup(reasonId);
-			reason = crlReason.toString();
+			try {
+				final int reasonId = revokedStatus.getRevocationReason();
+				final CRLReason crlReason = CRLReason.lookup(reasonId);
+				reason = crlReason.toString();
+			} catch (Exception e) {
+				reason = "unspecified";
+
+			}
 		} else if (certStatus instanceof UnknownStatus) {
 
 			if (logger.isInfoEnabled()) {
@@ -181,7 +186,7 @@ public class OCSPToken extends RevocationToken {
 	@Override
 	public String getAbbreviation() {
 		return "OCSPToken[" + DSSUtils.formatInternal(basicOCSPResp.getProducedAt()) + ", signedBy=" + (issuerToken == null ? "?" : issuerToken.getDSSIdAsString()) +
-				"]";
+			  "]";
 	}
 
 	@Override
